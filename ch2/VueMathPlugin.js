facc3d2c15c35922f0f085851e3291150ec55758
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-03-11 18:50:10 -0600

    updates to ES6 and adds sine, cosine, and tangent trig functions

--- ch2/VueMathPlugin.js
+++ ch2/VueMathPlugin.js
@@ -1,10 +1,23 @@
 export default {
     install: function(Vue) {
-        Vue.directive('square', function(el, binding) {
+        Vue.directive('square', (el, binding) => {
             el.innerHTML = Math.pow(binding.value, 2)
         })
-        Vue.directive('sqrt', function(el, binding){
+
+        Vue.directive('sqrt', (el, binding) => {
             el.innerHTML = Math.sqrt(binding.value)
+        })
+
+        Vue.directive('sine', (el, binding) => {
+            el.innerHTML = Math.sin(binding.value)
+        })
+
+        Vue.directive('cosine', (el, binding) => {
+            el.innerHTML = Math.cos(binding.value)
+        })
+
+        Vue.directive('tangent', (el, binding) => {
+            el.innerHTML = Math.tan(binding.value)
         })
     }
 }
--- ch2/main.js
+++ ch2/main.js
@@ -9,8 +9,21 @@ exports.default = {
         Vue.directive('square', function (el, binding) {
             el.innerHTML = Math.pow(binding.value, 2);
         });
+
         Vue.directive('sqrt', function (el, binding) {
             el.innerHTML = Math.sqrt(binding.value);
+        });
+
+        Vue.directive('sine', function (el, binding) {
+            el.innerHTML = Math.sin(binding.value);
+        });
+
+        Vue.directive('cosine', function (el, binding) {
+            el.innerHTML = Math.cos(binding.value);
+        });
+
+        Vue.directive('tangent', function (el, binding) {
+            el.innerHTML = Math.tan(binding.value);
         });
     }
 };
